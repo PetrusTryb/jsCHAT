8442180b03f74be4741e2f043a240f2ebd81a104
PetrusTryb <ptrybisz@gmail.com>
2019-07-08 23:23:03 +0200

    Praca nad powiadomieniami

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,6 +1,6 @@
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js');
+import('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+import('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+import('https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js');
 exports.handler = function(event, context, callback) {
 	var channel = event.headers["channel"];
     callback(null, {
