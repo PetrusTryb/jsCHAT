d7c3b873fe054eb340e7c0c7b93c29cba235a12b
PetrusTryb <ptrybisz@gmail.com>
2019-07-08 20:01:06 +0200

    Próba naprawy firebase-messaging-sw.js #2

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,3 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+firebase.initializeApp({
+  'messagingSenderId': '684850370153'
+});
 firebase.messaging().onMessage((payload) => {
   console.log('Message received. ', payload);
   // ...
