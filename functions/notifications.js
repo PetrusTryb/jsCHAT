5be1b055de28a2fe720427fc1f01bf11488b85ea
PetrusTryb <ptrybisz@gmail.com>
2019-07-08 23:28:40 +0200

    Import Firebase.js

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,7 +1,20 @@
-import('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-import('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
-import('https://www.gstatic.com/firebasejs/4.8.1/firebase-database.js');
+var firebase = require("firebase");
 exports.handler = function(event, context, callback) {
+	context.callbackWaitsForEmptyEventLoop = false;  //<---Important
+
+    var config = {
+    apiKey: "AIzaSyBdK0boacor04eHtkpaOt-o21n-TISqBTw",
+    authDomain: "jschat-official.firebaseapp.com",
+    databaseURL: "https://jschat-official.firebaseio.com",
+    projectId: "jschat-official",
+    storageBucket: "jschat-official.appspot.com",
+    messagingSenderId: "684850370153",
+    appId: "1:684850370153:web:4086f7f88148148d"
+    };
+
+    if(firebase.apps.length == 0) {   // <---Important!!! In lambda, it will cause double initialization.
+        firebase.initializeApp(config);
+    }
 	var channel = event.headers["channel"];
     callback(null, {
     statusCode: 200,
