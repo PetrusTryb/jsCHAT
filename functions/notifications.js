9a93f951873cb159b448644bf90317fde5683a71
PetrusTryb <ptrybisz@gmail.com>
2019-07-09 21:10:01 +0200

    Rezygnacja z użycia Firebase w back-endzie

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,24 +1,9 @@
-import * as admin from 'firebase-admin';
-
 exports.handler = function(event, context, callback) {
-	context.callbackWaitsForEmptyEventLoop = false;  //<---Important
-
-    /*var config = {
-    apiKey: "AIzaSyBdK0boacor04eHtkpaOt-o21n-TISqBTw",
-    authDomain: "jschat-official.firebaseapp.com",
-    databaseURL: "https://jschat-official.firebaseio.com",
-    projectId: "jschat-official",
-    storageBucket: "jschat-official.appspot.com",
-    messagingSenderId: "684850370153",
-    appId: "1:684850370153:web:4086f7f88148148d"
-    };
-
-    if(firebase.apps.length == 0) {   // <---Important!!! In lambda, it will cause double initialization.
-        firebase.initializeApp(config);
-    }*/
 	var channel = event.headers["channel"];
+	var sender = event.headers["sender"];
+	var receivers = event.headers["receivers"];
     callback(null, {
     statusCode: 200,
-    body: channel
+    body: "Kanał: "+channel+"<br/>Nadawca: "+sender+"<br/>Odbiorcy: "+receivers;
     });
 }
