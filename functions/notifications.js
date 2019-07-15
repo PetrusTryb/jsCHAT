ba857849584a94e4f410d0f8aae8064cbe72f8de
Piotr Trybisz <ptrybisz@gmail.com>
2019-07-15 23:45:37 +0200

    Update notifications.js

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,5 +1,5 @@
 exports.handler = function(event, context, callback) {
-	if(event.httpMethod=="POST"){
+	if(event.httpMethod!="OPTIONS"){
 	var channel = event.headers["channel"];
 	var sender = event.headers["sender"];
 	var receivers = event.headers["receivers"];
@@ -12,7 +12,8 @@ exports.handler = function(event, context, callback) {
 	});
     callback(null, {
     statusCode: 200,
-    body: "Kanał: "+channel+"<br/>Nadawca: "+sender+"<br/>Odbiorcy: "+receivers
+    body: "Kanał: "+channel+"<br/>Nadawca: "+sender+"<br/>Odbiorcy: "+receivers,
+    headers: {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"channel, sender, receivers"}
     });
 	}
 	else{
