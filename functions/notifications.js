010be92fc1bf6b05b5712a38cb8addcb61850c79
Piotr Trybisz <ptrybisz@gmail.com>
2019-07-16 12:57:52 +0200

    Update notifications.js

--- functions/notifications.js
+++ functions/notifications.js
@@ -1,28 +1,23 @@
 exports.handler = function(event, context, callback) {
 	if(event.httpMethod!="OPTIONS"){
-	var channel = event.headers["channel"];
-	var sender = event.headers["sender"];
-	var receivers = event.headers["receivers"];
-	/*console.log(channel);
-	console.log(sender);
-	console.log(receivers);*/
-	var splittedReceivers = receivers.split(";");
-	splittedReceivers.forEach(function(receiver){
-		//console.log(receiver);
-		var message = {
+	var channelId = event.headers["channelId"];
+	var channelName = event.headers["channelName"];
+	var senderNick = event.headers["senderNick"];
+	var channelSpecificReceivers = fetch("https://jschat-official.firebaseio.com/channels/"+channelId+"/permissions").then(resp => resp.json())
+    .then(resp => {
+        console.log(resp);
+    });
+		/*var message = {
     "to": receiver,
     "notification": {
       "title": channel,
       "body": "Nowa wiadomość od: "+sender,
       "click_action":"https://jschat.netlify.com/"
       }
-}
-		console.log(message);
-		/*
 	var req = new XMLHttpRequest();
 req.open('POST', 'https://fcm.googleapis.com/fcm/send', true);
 req.setRequestHeader("content-type", "application/json");
-req.setRequestHeader("authorization", "key=AAAAn3RDVmk:APA91bHlQhNV6WkrRt2KHXPwkKoG9HMQYmPLQ8rldqpP37B3-BNmwzT_zC796V2fmpPOjlRU3vHIbvHhpNXBBc34mwLduvaCCDPcaJcJae2P24bqPJgqXAs6qmG6lstJ6mNK7JRXlNqt");
+req.setRequestHeader("authorization", "key="+process.env.fcm_server_key);
 req.onreadystatechange = function (aEvt) {
   if (req.readyState == 4) {
      if(req.status == 200)
@@ -32,18 +27,17 @@ req.onreadystatechange = function (aEvt) {
   }
 };
 req.send(JSON.stringify());*/
-	});
 		
     callback(null, {
     statusCode: 200,
-    body: "Kanał: "+channel+"<br/>Nadawca: "+sender+"<br/>Odbiorcy: "+receivers,
-    headers: {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"channel, sender, receivers"}
+    body: "OK",
+    headers: {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"channelId, senderNick, channelName"}
     });
 	}
 	else{
 	callback(null, {
     statusCode: 204,
-    headers: {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"channel, sender, receivers"}
+    headers: {"Access-Control-Allow-Origin":"*","Access-Control-Allow-Headers":"channelId, senderNick, channelName"}
     });
 	}
 }
