4e955490d586163608f5196928909a5258240248
PetrusTryb <ptrybisz@gmail.com>
2020-05-01 01:01:03 +0200

    Added logging to User function

--- src/lambda/user.js
+++ src/lambda/user.js
@@ -1,32 +1,37 @@
 import db from './server'
 exports.handler = function(event, context, callback) {
     let body=JSON.parse(event.body);
-    console.log(body["sid"])
+    console.log("Validating session: "+body["sid"])
     db.collection("sessions").findOne({"sessionId":body["sid"]}, function(err, result) {
         if(result){
+            console.log("Session found: "+body["sid"]);
             let userId=result.uid;
             db.collection("users").findOne({"_id":userId},function(err,userData) {
                 if(userData){
+                    console.log("Session is valid!");
+                    db.close();
                     callback(null, {
                         statusCode: 200,
                         body: JSON.stringify(userData)
                     });
                 }
                 else{
+                    console.log("Invalid session, sorry.");
+                    db.close();
                     callback(null,{
                         statusCode:401,
                         body:"Invalid session"
                     });
                 }
-                db.close();
             })
         }
         else{
+            console.log("Session not found: "+body["sid"]);
+            db.close();
             callback(null, {
                 statusCode: 403,
                 body: "Not logged in"
               });
-            db.close();
         }
     })
 }
